f08c027eec097868015e0a8bda5d83b5fcd9287d
ittyi <vioiv.k4@gmail.com>
2024-02-12 19:54:45 +0900

    ✅ add: topページでHello Worldと、最低限のレスポンス200を確認するテストの追加

--- dayrepo/snippets/tests.py
+++ dayrepo/snippets/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.http import HttpRequest
+from snippets.views import top
 
 # Create your tests here.
 class TopPageViewTest(TestCase):
     def test_top_returns_200(self):
         request = HttpRequest()
+
+        res = top(request)
+        self.assertEqual(res.status_code, 200)
