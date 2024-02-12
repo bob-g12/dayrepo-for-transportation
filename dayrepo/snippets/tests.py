c1cae37f1e691596a53911d329824c28a70bf333
ittyi <vioiv.k4@gmail.com>
2024-02-12 20:37:01 +0900

    ✨ add: snippet_new のテスト追加

--- dayrepo/snippets/tests.py
+++ dayrepo/snippets/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 from django.http import HttpRequest
-from snippets.views import top
+from snippets.views import top, snippet_new
+from django.urls import resolve
 
 # Create your tests here.
-class TopPageViewTest(TestCase):
+class SnippetTopViewTest(TestCase):
     def test_top_returns_200(self):
-        request = HttpRequest()
-
-        res = top(request)
+        res = self.client.get("/") 
         self.assertEqual(res.status_code, 200)
+    
+    def test_should_resolve_snippet_top(self):
+        found = resolve("/")
+        self.assertEqual(top, found.func)
+
+class SnippetUpdateTest(TestCase):
+    def test_should_resolve_snippet_new(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new, found.func)
