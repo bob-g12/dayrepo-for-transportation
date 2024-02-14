18665e7f5421c5b53fd57d8796a26adc30055c6b
bob-g12 <sasakiT1019s@outlook.jp>
2024-02-14 20:56:10 +0900

    checklist/Car class作成

--- models.py
+++ models.py
@@ -21,3 +21,54 @@ class Account(models.Model):
     def __str__(self):
         
         return str(self.id)
+    
+class Car(models.Model):
+    
+    class Meta(object):
+        
+        #作成されるテーブル名を指定
+        db_table = 'car'
+        #管理画面でのモデルの名称
+        verbose_name = '車両'
+    #項目作成
+
+    id = models.AutoField(verbose_name='車両ID',primary_key=True,editable=False,max_length=6,blank=False, null=False)
+    vehicle_number = models.CharField(verbose_name='車両番号',max_length=15,blank=False, null=False)
+    now_mileage = models.IntegerField(verbose_name='走行距離',max_length=25,blank=False, null=False)
+    def __str__(self):
+        
+        return str(self.id)
+    
+class checklist(models.Model):
+    
+    class Meta(object):
+        
+        #作成されるテーブル名を指定
+        db_table = 'checklist'
+        #管理画面でのモデルの名称
+        verbose_name = '点検項目'
+    #項目作成
+
+    snippet_id = models.ForeignKey(Snippet, on_delete=models.CASCADE, null=False)
+    is_tire_damage = models.BooleanField(verbose_name='タイヤの損傷(空気圧/摩耗/亀裂/損傷)',default=False,blank=True, null=False)
+    is_tire_groove = models.BooleanField(verbose_name='タイヤの溝の深さ',default=False,blank=True, null=False)
+    is_tire_parts = models.BooleanField(verbose_name='タイヤのナット・ボルト(緩み/脱落)',default=False,blank=True, null=False)
+    is_radiator = models.BooleanField(verbose_name='ラジエータの冷却水(液量)',default=False,blank=True, null=False)
+    is_brake_oil = models.BooleanField(verbose_name='ブレーキオイル(液量)',default=False,blank=True, null=False)
+    is_air_tank = models.BooleanField(verbose_name='エアタンク(凝水)',default=False,blank=True, null=False)
+    is_air_brake = models.BooleanField(verbose_name='エアブレーキ(空気圧/排気音)',default=False,blank=True, null=False)
+    is_brake_pedal = models.BooleanField(verbose_name='ブレーキペダルの踏みしろ',default=False,blank=True, null=False)
+    is_parking_brake = models.BooleanField(verbose_name='駐車ブレーキ(かかり具合/引きしろ)',default=False,blank=True, null=False)
+    is_engine_oil = models.BooleanField(verbose_name='エンジンオイル(液量)',default=False,blank=True, null=False)
+    is_battery = models.BooleanField(verbose_name='バッテリー(液量)',default=False,blank=True, null=False)
+    is_belt = models.BooleanField(verbose_name='ファン・ベルトの異常(張り/損傷)',default=False,blank=True, null=False)
+    is_washer_fluid = models.BooleanField(verbose_name='ウォッシャー液/ワイパー(液量/噴射状態/払拭状態)',default=False,blank=True, null=False)
+    is_engine = models.BooleanField(verbose_name='エンジン(かかり具合/異音/状態)',default=False,blank=True, null=False)
+    is_light = models.BooleanField(verbose_name='ライト(ヘッドライト/ウインカー/車内灯/ハザード/速度表示)',default=False,blank=True, null=False)
+    is_brake_details = models.BooleanField(verbose_name='ブレーキチャンバロッド/ブレーキペダルのライニング',default=False,blank=True, null=False)
+    is_before_trouble = models.BooleanField(verbose_name='前日の異常',default=False,blank=False, null=False)
+    is_today_trouble = models.BooleanField(verbose_name='本日の異常',default=False,blank=False, null=False)
+    
+    def __str__(self):
+        
+        return str(self.id)
