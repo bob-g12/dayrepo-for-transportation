ce30d86d2a5e6aefe086adde4486f0910a57c4e2
ittyi <vioiv.k4@gmail.com>
2024-02-22 02:24:07 +0900

    ♻️ refactor: account系関数のclass化

--- dayrepo/batch/management/commands/baccount.py
+++ dayrepo/batch/management/commands/baccount.py
@@ -7,10 +7,25 @@ from snippets import models
 class Command(BaseCommand):
     help = "テストコマンド"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):               
+        print(args)
+        print(options)
+        print(options["get"])
+        print("options[\"get\"]:", options["get"] == "")
+        if options["get"] != None and options["get"] == "all": 
+            AccountCommand.get_account_all()
+            return
+    
+        print("一致するコマンドが存在しません。")
+        
+    def add_arguments(self, parser):
+        parser.add_argument('--get', nargs='?', default='', type=str)
+
+class AccountCommand:
+    def get_account_all():
         tmp = models.Account.objects.all()
         for i, t in enumerate(tmp):
-            print(i, "個目")
+            print(i+1, "個目")
             print("t.id:", t.id)
             print("t.first_name:", t.first_name)
             print("t.last_name:", t.last_name)
