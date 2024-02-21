aeecea29fc8de15236a2416101d45ecaf9ffba3c
bob-g12 <sasakiT1019s@outlook.jp>
2024-02-21 16:35:28 +0900

    テーブル位置入れ替え

--- dayrepo/snippets/models.py
+++ dayrepo/snippets/models.py
@@ -70,6 +70,66 @@ class Car(models.Model):
 
         return str(self.id)
 
+class Snippet(models.Model):
+
+    class Meta(object):
+
+        # 作成されるテーブル名を指定
+        db_table = "snippets"
+        # 管理画面でのモデルの名称
+        verbose_name = "日報"
+
+    # 項目作成
+
+    id = models.AutoField(
+        verbose_name="日報ID", primary_key=True, editable=True, blank=False, null=False,
+    )
+    account_id = models.ForeignKey(
+        Account, verbose_name="アカウントID", on_delete=models.DO_NOTHING, null=False, default=False
+    )
+    start_mileage = models.IntegerField(
+        verbose_name="出発時メーター", blank=False, null=False,
+    )
+    end_mileage = models.IntegerField(
+        verbose_name="到着時メーター", blank=False, null=False,
+    )
+    start_time = models.TimeField(
+        verbose_name="始業時間", blank=False, null=False, max_length=10,
+    )
+    end_time = models.TimeField(
+        verbose_name="終業時間", blank=False, null=False, max_length=10, 
+    )
+    start_point = models.CharField(
+        verbose_name="出発地点", blank=False, null=False, max_length=20
+    )
+    end_point = models.CharField(
+        verbose_name="最終到着地点", blank=False, null=False, max_length=20
+    )
+    break_spot = models.CharField(
+        verbose_name="休憩地点", blank=False, null=False, max_length=20
+    )
+    weather = models.CharField(
+        verbose_name="天気", max_length=5, blank=False, null=False,
+    )
+    gasoline_amount = models.FloatField(
+        verbose_name="給油", max_length=4, blank=True, null=True
+    )
+    oil = models.FloatField(verbose_name="オイル", max_length=4, blank=True, null=True)
+    is_trouble_situation = models.BooleanField(
+        verbose_name="事故/遅延等異常_状況", default=False, blank=True, null=False
+    )
+    is_trouble_cause = models.BooleanField(
+        verbose_name="事故/遅延等異常_原因", default=False, blank=True, null=False
+    )
+    is_trouble_support = models.BooleanField(
+        verbose_name="事故/遅延等異常_処置", default=False, blank=True, null=False
+    )
+    create_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
+
+    def __str__(self):
+
+        return str(self.id)
 
 class Checklist(models.Model):
 
@@ -83,7 +143,7 @@ class Checklist(models.Model):
     # 項目作成
 
     # snippet テーブルがまだ未定義のため、一時コメントアウト
-    # snippet_id = models.ForeignKey(Snippet, on_delete=models.CASCADE, null=False)
+    snippet_id = models.ForeignKey(Snippet, on_delete=models.CASCADE, null=False)
     is_tire_damage = models.BooleanField(
         verbose_name="タイヤの損傷(空気圧/摩耗/亀裂/損傷)",
         default=False,
@@ -170,64 +230,3 @@ class Checklist(models.Model):
 
         return str(self.snippet_id)
 
-
-class Snippet(models.Model):
-
-    class Meta(object):
-
-        # 作成されるテーブル名を指定
-        db_table = "snippets"
-        # 管理画面でのモデルの名称
-        verbose_name = "日報"
-
-    # 項目作成
-
-    id = models.AutoField(
-        verbose_name="日報ID", primary_key=True, editable=True, blank=False, null=False,
-    )
-    account_id = models.ForeignKey(
-        Account, verbose_name="アカウントID", on_delete=models.DO_NOTHING, null=False, default=False
-    )
-    start_mileage = models.IntegerField(
-        verbose_name="出発時メーター", blank=False, null=False,
-    )
-    end_mileage = models.IntegerField(
-        verbose_name="到着時メーター", blank=False, null=False,
-    )
-    start_time = models.TimeField(
-        verbose_name="始業時間", blank=False, null=False, max_length=10,
-    )
-    end_time = models.TimeField(
-        verbose_name="終業時間", blank=False, null=False, max_length=10, 
-    )
-    start_point = models.CharField(
-        verbose_name="出発地点", blank=False, null=False, max_length=20
-    )
-    end_point = models.CharField(
-        verbose_name="最終到着地点", blank=False, null=False, max_length=20
-    )
-    break_spot = models.CharField(
-        verbose_name="休憩地点", blank=False, null=False, max_length=20
-    )
-    weather = models.CharField(
-        verbose_name="天気", max_length=5, blank=False, null=False,
-    )
-    gasoline_amount = models.FloatField(
-        verbose_name="給油", max_length=4, blank=True, null=True
-    )
-    oil = models.FloatField(verbose_name="オイル", max_length=4, blank=True, null=True)
-    is_trouble_situation = models.BooleanField(
-        verbose_name="事故/遅延等異常_状況", default=False, blank=True, null=False
-    )
-    is_trouble_cause = models.BooleanField(
-        verbose_name="事故/遅延等異常_原因", default=False, blank=True, null=False
-    )
-    is_trouble_support = models.BooleanField(
-        verbose_name="事故/遅延等異常_処置", default=False, blank=True, null=False
-    )
-    create_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
-    update_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
-
-    def __str__(self):
-
-        return str(self.id)
