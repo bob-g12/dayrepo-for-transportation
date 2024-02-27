ea621938724636df5bd3a64224db22590dda0b4e
ittyi <vioiv.k4@gmail.com>
2024-02-28 00:52:05 +0900

    🔥 code clean

--- dayrepo/snippets/forms.py
+++ dayrepo/snippets/forms.py
@@ -34,25 +34,4 @@ class SnippetForm(forms.ModelForm):
             'break_time',
             'free_space',
             )
-        widgets = {
-            # 'account_id':forms.Select(
-            #     choices=(
-            #     ('','アカウントを選択'),
-            #     ('0','gokki'),
-            #     ('1','やんけ'),
-            #     ('2','とっしー'),
-            #     ),
-            #     attrs={
-            #     'required' : 'アカウントを選択'
-            # }),
-            # 'account_id':forms.Select(
-            #     choices=(
-            #     ('','車両を選択'),
-            #     ('札幌 ひ 20-20','0'),
-            #     ('1','船橋 ふ 19-19'),
-            #     ('2','八雲 や 89-89'),
-            #     ),
-            #     attrs={
-            #     'required' : '車両を選択'
-            # }),
-        }
+        widgets = {}
