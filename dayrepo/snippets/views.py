a6dfd1d3ae405f3f248e8d45617b3f2d22aa3a29
bob-g12 <sasakiT1019s@outlook.jp>
2024-03-02 22:58:11 +0900

    views更新

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -55,12 +55,11 @@ class SnippetView(View):
         # formに書いた内容を格納する
         form = SnippetForm(request.POST)
         form_trouble = DutiesTroubleForm(request.POST)
-        # 保存する前に一旦取り出す
-        post = form.save(commit=False)
-        post_trouble = form_trouble.save(commit=False)
+        form_process = ProcessForm(request.POST)
         # 保存
-        post.save()
-        post_trouble.save()
+        form.save()
+        form_trouble.save()
+        form_process.save()
         # トップ画面へ
         return redirect(to="snippet_list")
 
