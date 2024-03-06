49d5a92fed7540566bb8e1563bf83a6db790e1f1
bob-g12 <sasakiT1019s@outlook.jp>
2024-03-06 23:29:53 +0900

    すべてのページにstaticを供給する

--- dayrepo/dayrepo/settings.py
+++ dayrepo/dayrepo/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.static",
             ],
         },
     },
@@ -118,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
