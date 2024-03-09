94e0006066161b1b3a1d55a44f27f40ea42d17e5
ittyi <vioiv.k4@gmail.com>
2024-03-09 23:13:41 +0900

    ✨ add: 工程フォームと重複したデータのs剪定後、snippets の保存完了。後ほど利用するため、snippet_isも取得

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, QueryDict
 from django.views.generic import View
 from django.shortcuts import redirect
-from .models import Snippet
+from .models import Snippet, DutiesTrouble, Account, Car
 from .forms import SnippetForm, DutiesTroubleForm, ProcessForm
 
 
@@ -68,63 +68,64 @@ class SnippetView(View):
 
     # 投稿機能
     def post(self, request):
-        # formに書いた内容を格納する
         print(request.POST)
-        tmp = request.POST
-
-        form_duties_trouble = DutiesTroubleForm(request.POST)
-        res_duties_trouble = form_duties_trouble.save()
-        print("res_duties_trouble: ", res_duties_trouble)
-
-        # print(dict(tmp))
-        form_snippet = dict(tmp)
-        form_snippet_start_time = tmp.getlist("start_time")[0]
-        form_snippet["start_time"] = [form_snippet_start_time]
-
-        form_snippet_end_time = tmp.getlist("end_time")[0]
-        form_snippet["end_time"] = [form_snippet_end_time]
-
-        form_snippet_start_point = tmp.getlist("start_point")[0]
-        form_snippet["start_point"] = [form_snippet_start_point]
-
-        form_snippet_end_point = tmp.getlist("end_point")[0]
-        form_snippet["end_point"] = [form_snippet_end_point]
-
-        snippet_save = QueryDict()
-        # snippet_save[]
-        print(snippet_save)
-
-        #res_process = form_process.save()
-        form_process = dict(tmp)
-        form_process_count = len(tmp.getlist("via_point"))
+        req = request.POST
+        
+        # Snippets form
+        account = Account(
+            id = req.get("account_id"),
+        )
+        car = Car(
+            id = req.get("car_id"),
+        )
+        t = Snippet(
+            account_id=account,
+            car_id=car,
+            create_day=req.get("create_day"),
+            start_mileage=req.get("start_mileage"),
+            end_mileage=req.get("end_mileage"),
+            start_time=req.getlist("start_time")[0],
+            end_time=req.getlist("end_time")[0],
+            start_point=req.getlist("start_point")[0],
+            end_point=req.getlist("end_point")[0],
+            break_spot=req.get("break_spot"),
+            weather=req.get("weather"),
+            gasoline_amount=req.get("gasoline_amount"),
+            oil=req.get("oil"),
+            driving_time=req.get("driving_time"),
+            non_driving_time=req.get("non_driving_time"),
+            break_time=req.get("break_time"),
+            free_space=req.get("free_space"),
+        )
+        t.save()
+        snippet_id = t.pk
+        
+        
+        # DutiesTrouble form
+        # form_duties_trouble = DutiesTroubleForm(request.POST)
+        # form_duties_trouble.save()
+        # print("form_duties_trouble: ", form_duties_trouble.auto_id)
+        # res_duties_trouble = form_duties_trouble.save()
+        # print("res_duties_trouble: ", res_duties_trouble)
+        
+        
+        # Process form
+        form_process_count = len(req.getlist("via_point"))
         print(form_process_count)
         for i in range(form_process_count):
-            form_process_start_time = tmp.getlist("start_time")[i+1]
-            form_process["start_time"] = [form_process_start_time]
-            form_process_end_time = tmp.getlist("end_time")[i+1]
-            form_process["end_time"] = [form_process_end_time]
-            form_process_start_point = tmp.getlist("start_point")[i+1]
-            form_process["start_point"] = [form_process_start_point]
-            form_process_end_point = tmp.getlist("end_point")[i+1]
-            form_process["end_point"] = [form_process_end_point]
-            
-            form_process_via_point = tmp.getlist("via_point")[i]
-            form_process["via_point"] = [form_process_via_point]
-            form_process_client = tmp.getlist("client")[i]
-            form_process["client"] = [form_process_client]
-            form_process_goods = tmp.getlist("goods")[i]
-            form_process["goods"] = [form_process_goods]
-            form_process_load_situation = tmp.getlist("load_situation")[i]
-            form_process["load_situation"] = [form_process_load_situation]
-            form_process_is_load_situation = tmp.getlist("is_load_situation")[i]
-            form_process["is_load_situation"] = [form_process_is_load_situation]
-            form_process_load_mileage = tmp.getlist("load_mileage")[i]
-            form_process["load_mileage"] = [form_process_load_mileage]
-            form_process_load_situation = tmp.getlist("load_situation")[i]
-            form_process["load_situation"] = [form_process_load_situation]
+            form_process_start_time = req.getlist("start_time")[i + 1]
+            form_process_end_time = req.getlist("end_time")[i + 1]
+            form_process_start_point = req.getlist("start_point")[i + 1]
+            form_process_end_point = req.getlist("end_point")[i + 1]
+
+            form_process_via_point = req.getlist("via_point")[i]
+            form_process_client = req.getlist("client")[i]
+            form_process_goods = req.getlist("goods")[i]
+            form_process_load_situation = req.getlist("load_situation")[i]
+            form_process_is_load_situation = req.getlist("is_load_situation")[i]
+            form_process_load_mileage = req.getlist("load_mileage")[i]
+            form_process_load_situation = req.getlist("load_situation")[i]
             print()
-            print(form_process)
-        return
 
         # トップ画面へ
         return redirect(to="snippet_list")
