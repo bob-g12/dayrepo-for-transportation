9a9805230d9b84aeb1df5a48dee04951014e115b
bob-g12 <126374166+bob-g12@users.noreply.github.com>
2024-03-10 16:11:53 +0900

    Update dayrepo/snippets/static/js/process_add.js
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -1,7 +1,7 @@
 let i = 1 ;
 function addForm() {
   let content_area = document.getElementById("form_area");
-  var clone_element = content_area.cloneNode(true);
+  let clone_element = content_area.cloneNode(true);
   clone_element.id = i;
   alert("工程を追加しますか？");
   content_area.after(clone_element);
