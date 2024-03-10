f59416537c6226123940dcdec9c1bade36c6cf9a
bob-g12 <126374166+bob-g12@users.noreply.github.com>
2024-03-10 16:17:01 +0900

    Update dayrepo/snippets/views.py
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, QueryDict
+from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import redirect
 from .models import Account, Car, Snippet, DutiesTrouble, Checklist, Process
