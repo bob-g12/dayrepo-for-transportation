2f73e83b589983eb6004a61097edbd8462f2e530
bob-g12 <sasakiT1019s@outlook.jp>
2024-03-15 21:33:54 +0900

    add: Excelファイルの編集処理を追加

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -196,3 +196,31 @@ class ChecklistView(View):
         return redirect(to="snippet_post")
     
 checklist_post = ChecklistView.as_view()
+
+import openpyxl
+
+class Excel_View(View):
+
+    def index(request):
+        """
+        Excel output from template
+        """
+        # Excelのテンプレートファイルの読み込み
+        wb = openpyxl.load_workbook('docs\otamesi.xlsx')
+
+        sheet = wb['sheet1']
+        sheet['D4'] = '入力'
+        sheet['E2'] = 'new'
+
+        # Excelを返すためにcontent_typeに「application/vnd.ms-excel」をセットします。
+
+        response = HttpResponse(content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename=%s' % 'otamesi.xlsx'
+
+        # データの書き込みを行なったExcelファイルを保存する
+        wb.save(response)
+
+        # 生成したHttpResponseをreturnする
+        return response
+
+excel_editing = Excel_View.as_view()
