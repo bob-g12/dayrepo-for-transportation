ba32095fc98c10c372fd828071610f9d17c4ed13
bob-g12 <sasakiT1019s@outlook.jp>
2024-03-16 19:23:21 +0900

    fix: classビューやパスの入力方法に問題があったので修正/Excelファイルの編集機能は完成

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -199,28 +199,25 @@ checklist_post = ChecklistView.as_view()
 
 import openpyxl
 
-class Excel_View(View):
+    
+def excelfile_download(request):
+    """
+    Excel output from template
+    """
+    # Excelのテンプレートファイルの読み込み
+    wb = openpyxl.load_workbook('../docs/otamesi.xlsx')
 
-    def index(request):
-        """
-        Excel output from template
-        """
-        # Excelのテンプレートファイルの読み込み
-        wb = openpyxl.load_workbook('docs\otamesi.xlsx')
+    sheet = wb['Sheet1']
+    sheet['D2'] = '入力'
+    sheet['E2'] = 'ここ、E2'
+    # Excelを返すためにcontent_typeに「application/vnd.ms-excel」をセットします。
 
-        sheet = wb['sheet1']
-        sheet['D4'] = '入力'
-        sheet['E2'] = 'new'
+    response = HttpResponse(content_type='application/vnd.ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=%s' % 'otamesi.xlsx'
 
-        # Excelを返すためにcontent_typeに「application/vnd.ms-excel」をセットします。
+    # データの書き込みを行なったExcelファイルを保存する
+    wb.save(response)
 
-        response = HttpResponse(content_type='application/vnd.ms-excel')
-        response['Content-Disposition'] = 'attachment; filename=%s' % 'otamesi.xlsx'
-
-        # データの書き込みを行なったExcelファイルを保存する
-        wb.save(response)
-
-        # 生成したHttpResponseをreturnする
-        return response
-
-excel_editing = Excel_View.as_view()
+    # 生成したHttpResponseをreturnする
+    return response
+ 
