a1ce21cfe44af5abf324f161d3c9223adf4e1a51
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-01 21:46:24 +0900

    fix:工程フォームの追加機能を修正(途中)

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -1,9 +1,11 @@
 let i = 1 ;
 const addForm = () => {
   let content_area = document.getElementById("form_area");
+  let content_clone = document.getElementById("add_space");
   let clone_element = content_area.cloneNode(true);
   clone_element.id = i;
   alert("工程を追加しますか？");
-  content_area.after(clone_element);
+  content_area.reset();
+  content_clone.before(clone_element);
   i++ ;
 }
