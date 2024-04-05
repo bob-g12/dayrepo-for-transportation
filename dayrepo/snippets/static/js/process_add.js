41b27c5508b81cca82f68a3e150c84fbbe8066e0
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-05 19:04:42 +0900

    fix:現在のフォームの総数を受け取ることで上限を超えた際に追加の動作をしないよう設定/初期に表示しているフォームの入力を空にした上で複製することでコピー後に入力値もコピーしてしまわないよう改善

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -1,11 +1,27 @@
 let i = 1 ;
 const addForm = () => {
-  let content_area = document.getElementById("form_area");
-  let content_clone = document.getElementById("add_space");
-  let clone_element = content_area.cloneNode(true);
-  clone_element.id = i;
-  alert("工程を追加しますか？");
-  content_area.reset();
-  content_clone.before(clone_element);
-  i++ ;
+  let count = Number(document.getElementById("count").innerHTML);
+  let now_count = count + i;
+  if (now_count <= 6){
+    alert("工程を追加しますか？",);
+    let content_area = document.getElementById("form_area");
+    let clone_element = content_area.cloneNode(true);
+    let content_clone = document.getElementById("add_space");
+    let content_count = content_area.childElementCount;
+    for (let i=0; i<content_count; i++){
+      if (i != 8 && i <= 10){
+        clone_element.children[i].children[1].value="";
+      }
+      else if (i == 8){
+        clone_element.children[i].children[1].checked="";
+      }
+    }
+    content_clone.before(clone_element)
+    document.getElementById("count").innerHTML = now_count;
+    clone_element.id = now_count;
+    i++ ;
+  }
+  else {
+    alert("これ以上増やせません",);
+  }
 }
