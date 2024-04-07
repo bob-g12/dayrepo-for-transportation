b0fd2d7a35a370fa8b9ec89b8f78e212cfb10cc8
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-08 01:26:41 +0900

    fix:post画面へ推移する際に引数が必要となったためpostのみの推移パスを削除/パスの表記を簡潔なものに変更

--- dayrepo/snippets/urls.py
+++ dayrepo/snippets/urls.py
@@ -10,10 +10,9 @@ urlpatterns = [
     path("car/new/", views.car_registration,name="car_registration"),
     path("employee/", views.get_employee,name="get_employee"),
     path("list/", views.snippet_list,name="snippet_list"),
-    path("post/", views.snippet_post,name="snippet_post"),
     path("checklist/", views.checklist_post,name="checklist_post"),
     path("excel/<int:snippet_pk>/", views.excelfile_download,name="excelfile_download"),
-    path("checklist-edit/<int:post_id>/", views.checklist_edit,name="checklist_edit"),
+    path("checklist/edit/<int:post_id>/", views.checklist_edit,name="checklist_edit"),
     path("edit/<int:post_id>/", views.snippet_edit,name="snippet_edit"),
-    path("snippet-display/<int:checklist_id>/", views.snippet_post,name="snippet_display"),
+    path("snippet/post/<int:checklist_id>/", views.snippet_post,name="snippet_post"),
 ]
