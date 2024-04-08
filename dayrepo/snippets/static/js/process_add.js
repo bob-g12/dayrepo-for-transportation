3f3679b8b4be9699429bbe19ec83e45a0b7f1597
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-09 02:01:13 +0900

    jsファイルのif分岐を短く変更/上限を超えたら増えないように修正

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -3,26 +3,26 @@ const bool_select = 8;
 const not_select = 10;
 const addForm = () => {
   const count = Number(document.getElementById("count").innerHTML);
-  const now_count = count + 1;
-  if (now_count <= limit){
-    alert("工程を追加します");
-    const content_area = document.getElementById("form_area");
-    let clone_element = content_area.cloneNode(true);
-    let content_clone = document.getElementById("add_space");
-    const content_count = content_area.childElementCount;
-    for (let i=0; i<content_count; i++){
-      if (i != bool_select && i <= not_select){
-        clone_element.children[i].children[1].value="";
-      }
-      else if (i == bool_select){
-        clone_element.children[i].children[1].checked="";
-      }
+  let now_count = count;
+  if (now_count == limit){
+    alert("これ以上増やせません")
+    return
+  }
+  alert("工程を追加します");
+  now_count += 1;
+  const content_area = document.getElementById("form_area");
+  let clone_element = content_area.cloneNode(true);
+  let content_clone = document.getElementById("add_space");
+  const content_count = content_area.childElementCount;
+  for (let i=0; i<content_count; i++){
+    if (i != bool_select && i <= not_select){
+      clone_element.children[i].children[1].value="";
     }
-    content_clone.before(clone_element)
-    document.getElementById("count").innerHTML = now_count;
-    clone_element.id = now_count;
+    else if (i == bool_select){
+      clone_element.children[i].children[1].checked="";
+    }
   }
-  else {
-    alert("これ以上増やせません");
-  }
+  content_clone.before(clone_element)
+  document.getElementById("count").innerHTML = now_count;
+  clone_element.id = now_count;
 }
