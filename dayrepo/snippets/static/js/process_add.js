6885935fd15511b3f1330c7be793fbdd8a59d637
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-09 00:13:14 +0900

    fix:jsファイルの数値の意味が伝わらないので説明変数を作成/余分な処理を削除

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -1,25 +1,26 @@
-let i = 1 ;
+let limit = 6;
+let bool_select = 8;
+let not_select = 10;
 const addForm = () => {
   const count = Number(document.getElementById("count").innerHTML);
-  let now_count = count + i;
-  if (now_count <= 6){
+  let now_count = count + 1;
+  if (now_count <= limit){
     alert("工程を追加しますか？");
     const content_area = document.getElementById("form_area");
     let clone_element = content_area.cloneNode(true);
     let content_clone = document.getElementById("add_space");
     const content_count = content_area.childElementCount;
     for (let i=0; i<content_count; i++){
-      if (i != 8 && i <= 10){
+      if (i != bool_select && i <= not_select){
         clone_element.children[i].children[1].value="";
       }
-      else if (i == 8){
+      else if (i == bool_select){
         clone_element.children[i].children[1].checked="";
       }
     }
     content_clone.before(clone_element)
     document.getElementById("count").innerHTML = now_count;
     clone_element.id = now_count;
-    i++ ;
   }
   else {
     alert("これ以上増やせません");
