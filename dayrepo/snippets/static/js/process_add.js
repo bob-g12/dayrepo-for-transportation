77ebe8972ae34bb56e6d8574e811c55519feb56f
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-09 17:55:12 +0900

    jsファイルの変数名変更

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -11,12 +11,12 @@ const addForm = () => {
   let clone_element = content_area.cloneNode(true);
   const content_count = content_area.childElementCount;
   const bool_select = 8;
-  const not_select = 10;
+  const last_select = 10;
   for (let i=0; i<content_count; i++){
     if (i == bool_select){
       clone_element.children[i].children[1].checked="";
     }
-    else if (i <= not_select){
+    else if (i <= last_select){
       clone_element.children[i].children[1].value="";
     }
   }
