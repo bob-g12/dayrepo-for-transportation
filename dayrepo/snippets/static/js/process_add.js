856acb4bcdcfa5dd23e4ee0a34a35b03f159d4eb
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-09 20:29:46 +0900

    jsファイルフォーマット整理

--- dayrepo/snippets/static/js/process_add.js
+++ dayrepo/snippets/static/js/process_add.js
@@ -6,18 +6,20 @@ const addForm = () => {
   let count = Number(
     document.getElementById("count").innerHTML
   );
+
   const limit = 6;
   if (count >= limit){
     alert("これ以上増やせません")
     return
   }
+
   alert("工程を追加します");
   count += 1;
   const content_area = document.getElementById("form_area");
-  let clone_element = content_area.cloneNode(true);
   const content_count = content_area.childElementCount;
   const bool_select = 8;
   const last_select = 10;
+  let clone_element = content_area.cloneNode(true);
   for (let i=0; i<content_count; i++){
     if (i == bool_select){
       clone_element.children[i].children[1].checked="";
@@ -26,6 +28,7 @@ const addForm = () => {
       clone_element.children[i].children[1].value="";
     }
   }
+
   let content_clone = document.getElementById("add_space");
   content_clone.before(clone_element)
   document.getElementById("count").innerHTML = count;
