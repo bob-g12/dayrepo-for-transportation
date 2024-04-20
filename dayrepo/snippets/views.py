25452557f11b7f0f22082596f8c199da5db248d3
bob-g12 <126374166+bob-g12@users.noreply.github.com>
2024-04-20 22:08:37 +0900

    Update dayrepo/snippets/views.py
    Co-authored-by: ittyi <62760395+ittyi@users.noreply.github.com>

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -331,11 +331,11 @@ class DbDeletePattern():
             snippet_id=target_id
         )
         dutiestrouble.delete()
-        process = get_list_or_404(
+        process_list = get_list_or_404(
             Process, snippet_id=target_id
         )
-        for process_object in process:
-            process_object.delete()
+        for process in process_list:
+            process.delete()
 
         snippet = Snippet.objects.get(
             pk=target_id
