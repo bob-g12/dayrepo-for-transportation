39242b59e856b9a8f09c1d7b6d9ff7908622f421
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-20 21:22:05 +0900

    コメント修正

--- dayrepo/snippets/views.py
+++ dayrepo/snippets/views.py
@@ -348,7 +348,7 @@ class DbDeletePattern():
         )
 
         # 対象のsnippetが削除されたことを反映し、
-        # checklistのみの投稿として表示、判定させる
+        # 未提出日報として表示、判定させる
         checklist.is_snippet_make = False
         checklist.save()
 
