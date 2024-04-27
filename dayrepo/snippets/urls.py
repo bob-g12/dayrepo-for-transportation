0b5ef6cc1782f2942e25485489c8b71858a26282
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-27 16:47:39 +0900

    第二viewファイルの命名を変更

--- dayrepo/snippets/urls.py
+++ dayrepo/snippets/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from snippets import views, sub_views
+from snippets import views, master_views
 
 urlpatterns = [
     path("new/", views.snippet_new,name="snippet_new"),
@@ -16,5 +16,5 @@ urlpatterns = [
     path("edit/checklist/<int:checklist_id>/", views.checklist_edit,name="checklist_edit"),
     path("delete/<int:target_id>/<str:delete_type>/", views.db_delete,name="db_delete"),
     path("excel/<int:snippet_pk>/", views.excelfile_download,name="excelfile_download"),
-    path("list/vehicle/", sub_views.vehicle_list,name="vehicle_list"),
+    path("list/vehicle/", master_views.vehicle_list,name="vehicle_list"),
 ]
