bed8fa6f7ff9c68873b333025048a92ddbd642c7
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-27 16:29:18 +0900

    型ヒント追加

--- dayrepo/snippets/sub_views.py
+++ dayrepo/snippets/sub_views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpRequest
 from .models import Account, Car
 
 class VehicleListView(View):
-    def get(self, request):
+    def get(self, request: HttpRequest):
         cars = Car.objects.all()
 
         return render(
