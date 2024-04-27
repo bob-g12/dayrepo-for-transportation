6ab47409a536b1d0a55bb542bb9fa1f50aefcb66
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-27 16:53:07 +0900

    使うことを見越してimortしていたAccountをいったん削除

--- dayrepo/snippets/master_views.py
+++ dayrepo/snippets/master_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpRequest
-from .models import Account, Car
+from .models import Car
 
 class VehicleListView(View):
     def get(self, request: HttpRequest):
