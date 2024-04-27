f26116688970e0f311793e2fc2f9803a91cdc011
bob-g12 <sasakiT1019s@outlook.jp>
2024-04-27 16:12:31 +0900

    不要なコメント削除

--- dayrepo/snippets/sub_views.py
+++ dayrepo/snippets/sub_views.py
@@ -4,11 +4,8 @@ from .models import Account, Car
 
 class VehicleListView(View):
     def get(self, request):
-        # 記録してある投稿の全データを投稿時間を元にソートして表示
-        # snippets は存在する時点で提出済みとみなす
         cars = Car.objects.all()
 
-        # トップページのhtmlへ日報データをテンプレートに渡す
         return render(
             request,
             "vehicle_top_page.html",
