bc67aa7a59b8c95218f1c8558d3e19d60da6069d
bob-g12 <sasakiT1019s@outlook.jp>
2024-05-01 21:27:13 +0900

    一覧に表示するカラム値を変更

--- dayrepo/snippets/models.py
+++ dayrepo/snippets/models.py
@@ -112,7 +112,7 @@ class Car(models.Model):
 
     def __str__(self):
 
-        return f'{self.place_name},{self.class_number},{self.kana},{self.serial_number}'
+        return f'{self.id}{self.place_name},{self.class_number},{self.kana},{self.serial_number}'
 
 
 class Snippet(models.Model):
@@ -238,13 +238,7 @@ class Snippet(models.Model):
 
     def __str__(self):
 
-        return f'{self.id}, 
-        {self.checklist_id.account_id.last_name},
-        {self.checklist_id.account_id.first_name},
-        {self.checklist_id.car_id.place_name},
-        {self.checklist_id.car_id.class_number},
-        {self.checklist_id.car_id.kana},
-        {self.checklist_id.car_id.serial_number}'
+        return f'{self.id}, {self.checklist_id.account_id.last_name},{self.checklist_id.account_id.first_name},{self.checklist_id.car_id.place_name},{self.checklist_id.car_id.class_number},{self.checklist_id.car_id.kana},{self.checklist_id.car_id.serial_number}'
 
 class DutiesTrouble(models.Model):
 
@@ -290,10 +284,7 @@ class DutiesTrouble(models.Model):
 
     def __str__(self):
         
-        return f'{self.snippet_id},
-        {self.trouble_cause},
-        {self.trouble_situation},
-        {self.trouble_support}'
+        return f'{self.snippet_id},{self.trouble_cause},{self.trouble_situation},{self.trouble_support}'
 
 
 class Checklist(models.Model):
@@ -450,14 +441,7 @@ class Checklist(models.Model):
     )
     def __str__(self):
 
-        return f'{self.id},
-        {self.account_id.first_name},
-        {self.account_id.last_name},
-        {self.car_id.place_name},
-        {self.car_id.class_number},
-        {self.car_id.kana},
-        {self.car_id.serial_number},
-        {self.working_day},'
+        return f'{self.id},{self.account_id.first_name},{self.account_id.last_name},{self.car_id.place_name},{self.car_id.class_number},{self.car_id.kana},{self.car_id.serial_number},{self.working_day}'
 
 class Process(models.Model):
 
